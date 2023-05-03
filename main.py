65676b2c154d64e02058ff8d2541f2bc5cfda87f
sceuz <87309752+sceuz@users.noreply.github.com>
2023-05-03 20:20:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ client = commands.Bot(command_prefix='!?', intents = discord.Intents.all())
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
-client.run('ODkyODM3OTE0NDIwMDY0MzI5.YVSuGg.PS6wWot2sTbsdTt3pOCbp1GSBxM')
+client.run('TOKEN')
